f12edb321dfa701566f1b3ebb4a25851e3d1c5d7
Vassilis Lambropoulos <vlamp@iit.demokritos.gr>
2018-04-03 12:28:24 +0300

    Add a greeting test that does not pass, since I follow TDD.

---
+++ src/test/java/org/lampropoul/movierama/controllers/GreetingControllerTest.java
@@ -0,0 +1,11 @@
+package org.lampropoul.movierama.controllers;
+
+import org.junit.Test;
+
+public class GreetingControllerTest {
+
+    @Test
+    public void greeting() {
+        assert false;
+    }
+}
